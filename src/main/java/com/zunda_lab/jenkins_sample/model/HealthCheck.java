69a270f364667465b54729574ab7775397ed3d38
yuichmaeda <wmwm.gt.eplp@gmail.com>
2019-12-06 18:21:28 +0900

    travis-editted

--- src/main/java/com/zunda_lab/jenkins_sample/model/HealthCheck.java
+++ src/main/java/com/zunda_lab/jenkins_sample/model/HealthCheck.java
@@ -27,7 +27,7 @@ public class HealthCheck {
         String bodyType;
         if (bmi < BMI_NORMAL_LOWER_LIMIT) {
             bodyType = "低体重";
-            // FindBugsにわざと検知させるための行
+            // FindBugsにわざと検知させるための行yu
             bodyType.replace("低体重", "痩せ型");
             // String.replace()は置換した文字列を返却するため、使用する場合は以下が正解
             // bodyType = bodyType.replace("低体重", "痩せ型");
